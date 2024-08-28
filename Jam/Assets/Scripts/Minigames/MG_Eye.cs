a8ed59350802d66ebc57a1a064bae1d6b2e222e7
Keagon King <keagonk@gmail.com>
2024-08-28 10:43:04 -0500

    EyeballMiniGame Start

--- Jam/Assets/Scripts/Minigames/MG_Eye.cs
+++ Jam/Assets/Scripts/Minigames/MG_Eye.cs
@@ -1,9 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MG_Eye : MiniGameBase
 {
     [SerializeField] private List<GameObject> selectableEyes;
     [SerializeField] private GameObject lEye, rEye;
+
+
+
+    [SerializeField] private List<Sprite> irisColors;
+
+    public override void Enable()
+    {
+        base.Enable();
+        
+        RandomizeIrisColors();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (miniGameStage == Stage.Stage01)
+        {
+            // DO STAGE 1 STUFF HERE
+            // 1. REMOVE INFECTED EYE FROM EYE SOCKET
+        } else if (miniGameStage == Stage.Stage02)
+        {
+            // DO STAGE 2 STUFF HERE
+            // 1. MAKE UNINFECTED EYE MOVE AT RANDOM, FAST INTERVALS, TO RANDOM LOCATIONS
+            // 2. CHECK IF SELECTED GLASS EYE IS CORRECT COLOR
+            // 3. IF NOT, KNOCK DOWN TIME
+            // 4. IF SO , COMPLETE MINIGAME
+        }
+    }
+
+    public void RemoveEye()
+    {
+        SwapStage(2);
+    }
+
+
+    void RandomizeIrisColors()
+    {
+        for (int i = 0; i < selectableEyes.Count; i++)
+        {
+            selectableEyes[i].GetComponentInChildren<Iris>().gameObject.GetComponent<SpriteRenderer>().sprite = irisColors[Random.Range(0, irisColors.Count)];
+        }
+    }
 }
--- Jam/Assets/Scripts/Minigames/MiniGameBase.cs
+++ Jam/Assets/Scripts/Minigames/MiniGameBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,50 @@ public class MiniGameBase : MonoBehaviour
 
     public string miniGameInstructions;
     
-    public void Enable()
+    [SerializeField] internal Stage miniGameStage;
+    
+    public virtual void Enable()
     {
         gameObject.SetActive(true);
     }
 
-    public void Disable()
+    public virtual void Update()
+    {
+        
+    }
+
+    public virtual void Disable()
     {
         gameObject.SetActive(false);
     }
 
-    public void OnSuccess()
+    public virtual void OnSuccess()
     {
         piece.SetActive(true);
         gameObject.SetActive(false);
     }
+    
+    public virtual void SwapStage(int num)
+    {
+        switch (num)
+        {
+            case 1:
+                miniGameStage = Stage.Stage01;
+                break;
+            case 2:
+                miniGameStage = Stage.Stage02;
+                break;
+            case 3:
+                miniGameStage = Stage.Stage03;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+enum Stage {
+    Stage01,
+    Stage02,
+    Stage03
 }
