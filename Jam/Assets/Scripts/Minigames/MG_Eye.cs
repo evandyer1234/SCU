ecc3f0c83d791258695bd5492b1629616364fbdd
Keagon King <kaking2@student.fullsail.edu>
2024-08-28 08:29:03 -0500

    commit 1 - layout

---
+++ Jam/Assets/Scripts/Minigames/MG_Eye.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Eye : MiniGameBase
+{
+    [SerializeField] private List<GameObject> selectableEyes;
+    [SerializeField] private GameObject lEye, rEye;
+}
