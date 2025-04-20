7ecd6653b8a9f782c265ff12bc4b47c9449a4713
Alexander Blehm <blehm.ar@gmail.com>
2025-04-20 19:29:00 +0200

    Add Scene and Tag Constants

---
+++ Jam/Assets/Scripts/Helpers/NamingConstants.cs
@@ -0,0 +1,9 @@
+namespace Helpers
+{
+    public class NamingConstants
+    {
+        public static string TAG_MINIGAME_HEARTSTRING = "MinigameHeartstring";
+        public static string TAG_PLAYER = "Player";
+        public static string TAG_EYE_SLOT = "EyeSlot";
+    }
+}
