705cf7de6998b2c3624f439f0ec982b948b5d754
toffl1 <fhaytr63@gmail.com>
2025-04-24 23:48:30 +0200

    normal cursor reactivates outside of game window

--- Jam/Assets/Scripts/UI/CustomCursor.cs
+++ Jam/Assets/Scripts/UI/CustomCursor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class CustomCursor : MonoBehaviour
 {
@@ -16,5 +17,29 @@ public class CustomCursor : MonoBehaviour
         _targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         _targetPos = new Vector3 (_targetPos.x, _targetPos.y, 10f);
         transform.position = _targetPos;
+
+        Cursor.visible = !MouseScreenCheck();
+    }
+
+    //https://discussions.unity.com/t/detect-when-mouse-leaves-the-screen-area/630801/3
+    bool MouseScreenCheck()
+    {
+
+        #if UNITY_EDITOR
+            if (Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Handles.GetMainGameViewSize().x - 1 || Input.mousePosition.y >= Handles.GetMainGameViewSize().y - 1)
+            {
+                return false;
+            }
+        
+        #else
+            
+            if (Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Screen.width - 1 || Input.mousePosition.y >= Screen.height - 1)
+            {
+            return false;
+            }
+
+        #endif
+        
+        return true;
     }
 }
