45f01ec4a963f2518657751af37b44c35714a6f7
Alexander Blehm <blehm.ar@gmail.com>
2025-05-06 19:18:32 +0200

    Add click hints for heartstring minigame

--- Jam/Assets/Scripts/Minigames/HeartString/MG_HeartString.cs
+++ Jam/Assets/Scripts/Minigames/HeartString/MG_HeartString.cs
@@ -1,4 +1,5 @@
 using Helpers;
+using UI;
 using UnityEngine;
 
 public class MG_HeartString : MiniGameBase
@@ -6,6 +7,9 @@ public class MG_HeartString : MiniGameBase
 
     [SerializeField, Tooltip("A reference to the used icons on the top")] 
     private SpriteRenderer[] _icons;
+    
+    [SerializeField, Tooltip("The hints for which to display clicking hints")]
+    private InteractionHint[] _veinOutlineHints;
     
     [SerializeField, Tooltip("A reference to the corruption icon")] 
     private Sprite _corruptionIcon;
@@ -20,6 +24,16 @@ public class MG_HeartString : MiniGameBase
         
         _correctVeinNumber = Random.Range(0, _icons.Length);
         _icons[_correctVeinNumber].sprite = _corruptionIcon;
+        
+        Invoke(nameof(AnimateVeinClickAreas), 2f);
+    }
+
+    private void AnimateVeinClickAreas()
+    {
+        foreach (var _veinHint in _veinOutlineHints)
+        {
+            _veinHint.TriggerAnimation();
+        }
     }
 
     public void Selection(int index)
