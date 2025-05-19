c9cd2364945af51846aa6ac3e96f48d6489d458c
toffl1 <fhaytr63@gmail.com>
2025-05-19 23:28:12 +0200

    added fade in
    on scene launch (toffl UI proto)

---
+++ Jam/Assets/Scripts/UI/SceneFade.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFade : MonoBehaviour
+{   
+    public float fadeSpeed;
+
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    private float _currentOpacity;
+    private float _targetOpacity;
+
+    void Start()
+    {
+        FadeIn();
+    }
+
+    void Update()
+    {
+        _currentOpacity = Mathf.MoveTowards(_currentOpacity, _targetOpacity, fadeSpeed * Time.deltaTime);
+
+        _canvasGroup.alpha = _currentOpacity;
+
+        if(_currentOpacity == _targetOpacity)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void FadeIn()
+    {
+        _currentOpacity = 1f;
+        _targetOpacity = 0f;
+    }
+}
