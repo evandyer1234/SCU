d8bc8a55a957d83943bca1f8a6cf6debcb03cb2e
toffl1 <fhaytr63@gmail.com>
2025-05-23 01:34:53 +0200

    placed fade on separate sorting layer
    to make sure it is always in front of ALL other objects in the scene (including tooltip)

--- Jam/Assets/Scripts/UI/HoverTooltip.cs
+++ Jam/Assets/Scripts/UI/HoverTooltip.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.UI;
+using TMPro;
 
 public class HoverTooltip : MonoBehaviour
-{
+{   
+    [TextArea(5,20)]
+    [SerializeField] private string _textToDisplay;
+
     [SerializeField] private GameObject _tooltip;
+    [SerializeField] private TMP_Text _TMP;
     [SerializeField] private GameObject _canvasGroup;
 
     private void Start()
