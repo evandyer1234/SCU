71be2c5998ee3a5ae749c422a80ebf923ac53765
toffl1 <fhaytr63@gmail.com>
2025-05-23 02:04:53 +0200

    added fade animation to tooltip

--- Jam/Assets/Scripts/UI/HoverTooltip.cs
+++ Jam/Assets/Scripts/UI/HoverTooltip.cs
@@ -2,31 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.UI;
-using TMPro;
 
 public class HoverTooltip : MonoBehaviour
 {   
-    [TextArea(5,20)]
-    [SerializeField] private string _textToDisplay;
-
     [SerializeField] private GameObject _tooltip;
-    [SerializeField] private TMP_Text _TMP;
-    [SerializeField] private GameObject _canvasGroup;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    public float fadeSpeed;
+
+    private bool _fading = false;
+    private bool _waitingToDisable = false;
+
+    private float _currentOpacity;
+    private float _targetOpacity;
 
     private void Start()
     {
         _tooltip.SetActive(false);
     }
 
+    private void Update()
+    {
+        if(_fading)
+        {
+            _currentOpacity = Mathf.MoveTowards(_currentOpacity, _targetOpacity, fadeSpeed * Time.deltaTime);
+            
+            _canvasGroup.alpha = _currentOpacity;
+
+            if(Mathf.Approximately(_currentOpacity, _targetOpacity))
+            {
+                _fading = false;
+
+                if(_waitingToDisable)
+                {
+                    _tooltip.SetActive(false);
+                }
+            }
+        }
+    }
+
     private void OnMouseEnter()
     {
-        //Debug.Log("hover");
         _tooltip.SetActive(true);
+        _targetOpacity = 1f;
+        _currentOpacity = 0f;
+        _fading = true;
+        _waitingToDisable = false;
+        _canvasGroup.alpha = _currentOpacity;
     }
 
     private void OnMouseExit()
     {
-        //Debug.Log("mouse exit");
-        _tooltip.SetActive(false);
+        _targetOpacity = 0f;
+        _currentOpacity = 1f;
+        _fading = true;
+        _waitingToDisable = true;
+        _canvasGroup.alpha = _currentOpacity;
     }
 }
