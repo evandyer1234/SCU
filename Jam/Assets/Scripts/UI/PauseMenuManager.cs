06cf305507d467257648c93e39fe090fcee83eca
EatenBaguette <esb1212@icloud.com>
2025-06-07 21:28:58 -0400

    correct time.timescale to float

--- Jam/Assets/Scripts/UI/PauseMenuManager.cs
+++ Jam/Assets/Scripts/UI/PauseMenuManager.cs
@@ -52,7 +52,7 @@ public class PauseMenuManager : MonoBehaviour
         _pauseMenu.SetActive(false);
         _paused = false;
         //AkSoundEngine.SetRTPCValue("isPaused", 0, null);
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
 
         EnableAllTooltipsInScene(true);
     }
@@ -79,7 +79,7 @@ public class PauseMenuManager : MonoBehaviour
         _pauseMenu.SetActive(true);
         _paused = true;
         //AkSoundEngine.SetRTPCValue("isPaused", 1, null);
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
 
         if (_gameOver)
         {
