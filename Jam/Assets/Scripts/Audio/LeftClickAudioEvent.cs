534aebdaac2b4de8eb7b8243811a175d0ba8eed9
EatenBaguette <esb1212@icloud.com>
2025-07-10 15:52:09 -0400

    adjust general click and release sfx, design and implement button hover sfx

--- Jam/Assets/Scripts/Audio/LeftClickAudioEvent.cs
+++ Jam/Assets/Scripts/Audio/LeftClickAudioEvent.cs
@@ -6,8 +6,10 @@ public class LeftClickAudioEvent : MonoBehaviour
 {
     private PauseMenuManager _pauseMenuManager;
     private SCUInputAction _scuInputAction;
+    [SerializeField] private AK.Wwise.Event mouseOverEvent;
     [SerializeField] private AK.Wwise.Event leftClickedEvent;
     [SerializeField] private AK.Wwise.Event leftReleasedEvent;
+    [SerializeField] private bool playDuringPause = false;
 
     void Awake()
     {
@@ -17,18 +19,37 @@ public class LeftClickAudioEvent : MonoBehaviour
             .GetComponent<PauseMenuManager>();
     }
 
+    void Start()
+    {
+        if (_pauseMenuManager == null)
+        {
+            _pauseMenuManager = GameObject.FindGameObjectWithTag(NamingConstants.TAG_PAUSE_MENU_MANAGER)
+                .GetComponent<PauseMenuManager>();
+        }
+    }
+
     private void OnMouseOver()
     {
-        if (_pauseMenuManager.isGamePaused()) return;
+        if (_pauseMenuManager.isGamePaused() && !playDuringPause) return;
 
-        if (MouseInput.LeftClicked(_scuInputAction))
+        if (MouseInput.LeftClicked(_scuInputAction) && leftClickedEvent != null)
         {
             leftClickedEvent.Post(gameObject);
         }
 
-        if (MouseInput.LeftReleased(_scuInputAction))
+        if (MouseInput.LeftReleased(_scuInputAction) && leftReleasedEvent != null)
         {
             leftReleasedEvent.Post(gameObject);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (_pauseMenuManager.isGamePaused() && !playDuringPause) return;
+        
+        if (mouseOverEvent != null)
+        {
+            mouseOverEvent.Post(gameObject);
         }
     }
 }
